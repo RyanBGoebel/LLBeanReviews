84255ce1f0d1a4cc4531a12d041693252f64255a
Ryan Goebel <68959182+RyanBGoebel@users.noreply.github.com>
2022-06-12 18:23:56 -0400

    Delete execute_all.R

--- execute_all.R
+++
@@ -1,8 +0,0 @@
-# ==============================================================================
-# EXECUTE FILES IN ORDER
-# ==============================================================================
-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-
-source("scrape_data_import.R")
-source("model_classification.R")
-source("bean_textMining.R")
